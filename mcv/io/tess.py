71f50287624b74a25449b95d5c6b7ff893b8efd5
Hannu Parviainen <hannu@iac.es>
2022-02-17 12:21:25 +0000

    Improved TESS input.

--- mcv/io/tess.py
+++ mcv/io/tess.py
@@ -5,23 +5,70 @@ from typing import Union, Optional, List
 from astropy.io.fits import getval
 from astropy.stats import mad_std
 from astropy.table import Table
-from numpy import nanmedian, zeros, arange, array, diff, concatenate, sqrt, ones, inf
+from numpy import nanmedian, zeros, arange, array, diff, concatenate, sqrt, ones, inf, median
 from pytransit.orbits import fold
 from pytransit.utils.keplerlc import KeplerLC
 from uncertainties import nominal_value
 
 from .photometry import Photometry
 
-def file_filter(f, partial_tic, sectors):
-    if 'qlp' in f.name:
-        sector, tic = f.name.split('_')[4].split('-')
-    else:
-        _, sector, tic, _, _ = f.name.split('-')
 
-    if sectors != 'all':
-        return int(sector[1:]) in sectors and str(partial_tic) in tic
+def identify_tess_format_and_sector(f: Path):
+    f = Path(f)
+    if 'hlsp_qlp' in f.name:
+        fmt = 'QLP'
+        sector = int(f.name.split('_')[4].split('-')[0][1:])
+    elif 'hlsp_tess-spoc' in f.name:
+        fmt = 'TESS-SPOC'
+        sector = int(f.name.split('_')[4].split('-')[1][1:])
+    elif f.name[:6] == 'tess20':
+        fmt = 'SPOC'
+        sector = int(f.name.split('-')[1][1:])
     else:
-        return str(partial_tic) in tic
+        fmt = None
+        sector = None
+    return f, fmt, sector
+
+
+def get_tess_files(d: Path):
+    files = [identify_tess_format_and_sector(f) for f in Path(d).glob('*.fits')]
+    return sorted(files, key=lambda f:f[2])
+
+
+def read_qlp(f: Path, use_pdc: bool = True):
+    tb = Table.read(f)
+    m = tb['QUALITY'] == 0
+    time = tb['TIME'].data[m] + tb.meta['BJDREFI']
+    flux = tb['KSPSAP_FLUX'].data[m].astype('d') if use_pdc else tb['SAP_FLUX'].data[m].astype('d')
+    return time, flux / median(flux), tb.meta['TIMEDEL']
+
+
+def read_tess_spoc(f: Path, use_pdc: bool = True):
+    tb = Table.read(f)
+    m = tb['QUALITY'] == 0
+    time = array(tb['TIME'].data[m] + tb.meta['BJDREFI'])
+    flux = array(tb['PDCSAP_FLUX'].data[m].astype('d') if use_pdc else tb['SAP_FLUX'].data[m].astype('d'))
+    flux /= median(flux)
+    if use_pdc:
+        contamination = 1 - tb.meta['CROWDSAP']
+        flux = contamination + (1 - contamination) * flux
+    return time, flux, tb.meta['TIMEDEL']
+
+
+def read_spoc(f: Path, use_pdc: bool = True):
+    tb = Table.read(f)
+    m = tb['QUALITY'] == 0
+    time = array(tb['TIME'].data[m] + tb.meta['BJDREFI'])
+    flux = array(tb['PDCSAP_FLUX'].data[m].astype('d') if use_pdc else tb['SAP_FLUX'].data[m].astype('d'))
+    flux /= median(flux)
+    if use_pdc:
+        contamination = 1 - tb.meta['CROWDSAP']
+        flux = contamination + (1 - contamination) * flux
+    return time, flux, tb.meta['TIMEDEL']
+
+
+tess_readers = {'SPOC': read_spoc, 'TESS-SPOC': read_tess_spoc, 'QLP': read_qlp}
+
 
 def read_tess(tic: int, datadir: Union[Path, str],
               sectors: Optional[Union[List[int], str]] = 'all',
@@ -30,25 +77,11 @@ def read_tess(tic: int, datadir: Union[Path, str],
               transit_duration: float = 0.1, baseline_duration: float = 0.3,
               depth: float = 0.0, sigma_low: float = 5.0, sigma_high: float= 5.0):
 
-    files = [f for f in sorted(Path(datadir).glob('*tess*lc.fits')) if file_filter(f, tic, sectors)]
+    files = get_tess_files(datadir)
 
     times, fluxes, sectors, exptimes, nsamples = [], [], [], [], []
-    for dfile in files:
-        tb = Table.read(dfile)
-
-        if 'PDCSAP_FLUX' in tb.colnames:
-            source = 'SPOC'
-            fcol = 'PDCSAP_FLUX' if use_pdc else 'SAP_FLUX'
-        elif 'KSPSAP_FLUX' in tb.colnames:
-            source = 'QLP'
-            fcol = 'KSPSAP_FLUX'
-
-        bjdrefi = tb.meta['BJDREFI']
-        df = tb.to_pandas().dropna(subset=['TIME', fcol])
-        time = df.TIME.values + bjdrefi
-        flux = df[fcol].values
-        flux /= nanmedian(flux)
-
+    for (fname, fmt, sector) in files:
+        time, flux, exptime = tess_readers[fmt](fname, use_pdc)
         wn = mad_std(diff(flux)) / sqrt(2)
 
         m = (flux >= 1 - depth - sigma_low*wn) & (flux <= 1 + sigma_high*wn)
@@ -66,13 +99,10 @@ def read_tess(tic: int, datadir: Union[Path, str],
             times.append(time[m])
             cfluxes = [flux[m]]
 
-        if use_pdc and 'CROWDSAP' in tb.meta:
-            contamination = 1 - tb.meta['CROWDSAP']
-            cfluxes = [contamination + (1 - contamination) * f for f in cfluxes]
         fluxes.extend(cfluxes)
-        sectors.extend(len(cfluxes)*[getval(dfile, 'sector')])
-        exptimes.extend(len(cfluxes)*[0.0 if source == 'SPOC' else 0.021])
-        nsamples.extend(len(cfluxes)*[1 if source == 'SPOC' else 10])
+        sectors.extend(len(cfluxes)*[sector])
+        exptimes.extend(len(cfluxes)*[exptime])
+        nsamples.extend(len(cfluxes)*[max(1, int(exptime / 0.0013889))])
 
     instrument = len(times) * ["TESS"]
     segments = list(arange(len(times)))
